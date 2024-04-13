2186c98320eb7933441a69b9c05dc9d3c93eaeb3
VitaliyKosh <zs.101101@yandex.ru>
2024-04-14 00:58:22 +0300

    save api key

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { $router } from './router/message.router.js';
 import { Bot } from './bot/bot.js';
+import dotenv from 'dotenv'
 
-const API_KEY_BOT = '7007641309:AAH66lp5YWih0xbz4ArtwrqwhX_im3J0c_E';
+dotenv.config(); 
 
-const bot = new TelegramBot(API_KEY_BOT, {
+const bot = new TelegramBot(process.env.API_KEY_BOT, {
   polling: true
 });
 
